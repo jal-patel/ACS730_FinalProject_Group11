a29b008d48e075cc624759ce51aaba64911fd5a6
jal-patel <129141197+jal-patel@users.noreply.github.com>
2023-04-14 19:19:33 -0400

    Update variables.tf

--- modules/aws_network/variables.tf
+++ modules/aws_network/variables.tf
@@ -3,3 +3,25 @@
 #TO DO = Define ami Variable with type string
 #TO DO = Define instance_type Variable with type string
 #TO DO = Define key_name Variable with type string
+
+# Define variables
+variable "environment" {
+  default = "Development"
+  type    = string
+}
+variable "group_name" {
+  default = "Group11"
+  type    = string
+}
+variable "ami" {
+  default = "ami-06e46074ae430fba6"
+  type    = string
+}
+variable "instance_type" {
+  default = "t2.micro"
+  type    = string
+}
+variable "key_name" {
+  default = "access"
+  type    = string
+}
