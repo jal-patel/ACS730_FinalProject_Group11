9fedce4fb1f3a009f7e93460d16b77d13594bd25
jal-patel <129141197+jal-patel@users.noreply.github.com>
2023-04-14 19:26:51 -0400

    Update variables.tf

---
+++ project/Development/network/variables.tf
@@ -0,0 +1,21 @@
+# Define variables
+variable "environment" {
+  default = "Development"
+  type    = string
+}
+variable "group_name" {
+  default = "Group11"
+  type    = string
+}
+variable "ami" {
+  default = "ami-06e46074ae430fba6"
+  type    = string
+}
+variable "instance_type" {
+  default = "t2.micro"
+  type    = string
+}
+variable "key_name" {
+  default = "access"
+  type    = string
+}
