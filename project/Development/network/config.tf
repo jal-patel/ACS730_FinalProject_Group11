e9cfa68899d058d9394d0ceb69eb180ea93ec645
jal-patel <129141197+jal-patel@users.noreply.github.com>
2023-04-14 19:21:56 -0400

    Update config.tf

--- project/Development/network/config.tf
+++ project/Development/network/config.tf
@@ -1,1 +1,9 @@
 #TO DO = Create terraform configuration for S3.
+
+terraform {
+  backend "s3" {
+    bucket = "project-01-bucket"                    // Bucket where to SAVE Terraform State
+    key    = "DevelopmentNetwork/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    region = "us-east-1"                            // Region where bucket is created
+  }
+}
